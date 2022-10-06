82597d54499b5525439a4fb3b7b4285a0c0fdc5d
only丶炀 <1763645020@qq.com>
2022-10-06 20:59:02 +0800

    ui: 侧边栏布局

--- src/components/explorer/modules/sidebar/sidebar.vue
+++ src/components/explorer/modules/sidebar/sidebar.vue
@@ -1,14 +1,39 @@
 <template>
-<div class="explorer-top-sidebar-wrapper">
-  <h1>侧边栏</h1>
+<div class="explorer-left-sidebar-wrapper">
+  <div class="sidebar-stick-wrapper">
+    置顶区域
+  </div>
+  <div class="sidebar-menu-wrapper">
+    <n-tree block-line expand-on-click default-expand-all	 :data="menuTree"></n-tree>
+  </div>
 </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import { TreeOption } from 'naive-ui';
+
 export default defineComponent ( {
   name: 'explorer-sidebar',
+  setup () {
+
+    const menuTree: TreeOption[] = [
+      {
+        label: '根目录',
+        children: [
+          {
+            label: '一级目录',
+          },
+        ],
+      },
+    ];
+
+    return {
+      menuTree,
+    };
+
+  },
 } );
 </script>
 
