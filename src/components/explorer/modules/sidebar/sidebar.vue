aaa62748f5a825eece574aed43a84097f32cdb4e
only丶炀 <1763645020@qq.com>
2022-10-08 23:19:58 +0800

    ui: 侧边栏布局完成

--- src/components/explorer/modules/sidebar/sidebar.vue
+++ src/components/explorer/modules/sidebar/sidebar.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="explorer-left-sidebar-wrapper">
   <div class="sidebar-stick-wrapper">
-    置顶区域
+    <n-tree block-line expand-on-click selectable default-expand-all :data="menuTree2"></n-tree>
   </div>
   <div class="sidebar-menu-wrapper">
-    <n-tree block-line expand-on-click default-expand-all	 :data="menuTree"></n-tree>
+    <n-tree block-line expand-on-click selectable :data="menuTree"></n-tree>
   </div>
 </div>
 </template>
@@ -21,9 +21,54 @@ export default defineComponent ( {
     const menuTree: TreeOption[] = [
       {
         label: '根目录',
+        key: 'root-1',
         children: [
           {
             label: '一级目录',
+            key: 'root-1-1',
+            children: [
+              {
+                label: '一级子目录1',
+                key: 'root-1-1-1',
+              },
+            ],
+          },
+          {
+            label: '二级目录',
+            key: 'root-1-2',
+          },
+          {
+            label: '三级目录',
+            key: 'root-1-3',
+          },
+          {
+            label: '四级目录',
+            key: 'root-1-4',
+          },
+        ],
+      },
+    ];
+
+    const menuTree2: TreeOption[] = [
+      {
+        label: '快速访问',
+        key: 'root-1',
+        children: [
+          {
+            label: '快速访问一',
+            key: 'root-1-1',
+          },
+          {
+            label: '快速访问二',
+            key: 'root-1-2',
+          },
+          {
+            label: '快速访问三',
+            key: 'root-1-3',
+          },
+          {
+            label: '快速访问四',
+            key: 'root-1-4',
           },
         ],
       },
@@ -31,6 +76,7 @@ export default defineComponent ( {
 
     return {
       menuTree,
+      menuTree2,
     };
 
   },
