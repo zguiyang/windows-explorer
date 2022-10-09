40e6947684e50b8e2d788fbad1bf81140e631984
only丶炀 <1763645020@qq.com>
2022-10-09 08:46:40 +0800

    feat: 文件列表区域更新

--- src/components/explorer/modules/files-container/container.vue
+++ src/components/explorer/modules/files-container/container.vue
@@ -1,12 +1,22 @@
 <template>
-<h1>内容区域</h1>
+<div class="explorer-right-files-container-wrapper">
+  <table-list></table-list>
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import TableList from './table-list.vue';
+
 export default defineComponent ( {
   name: 'explorer-container',
+  components: { TableList },
+  setup () {
+
+    // do nothing
+
+  },
 } );
 </script>
 
