71f9a1620eea08fdf47a78dfd17b7a979c904d8d
only丶炀 <1763645020@qq.com>
2022-10-12 08:26:42 +0800

    upd: 文件列表区域增加空数据提示

--- src/components/explorer/modules/files-container/table-list/index.vue
+++ src/components/explorer/modules/files-container/table-list/index.vue
@@ -1,16 +1,30 @@
 <template>
 <div class="files-container-table-list">
-<n-data-table :columns="tableColumns" :data="tableData"></n-data-table>
+<n-data-table :columns="tableColumns" :data="tableData">
+  <template #empty>
+    <n-space vertical :size="12">
+      <n-p style="text-align: center;">
+        <n-icon :size="32">
+          <archive16-regular></archive16-regular>
+        </n-icon>
+      </n-p>
+      <n-p>暂无数据，你可以点击左上角的新建按钮创建资源</n-p>
+    </n-space>
+  </template>
+</n-data-table>
 </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import { Archive16Regular } from '@vicons/fluent';
+
 import { useTableListData } from './hooks';
 
 export default defineComponent ( {
   name: 'table-list',
+  components: { Archive16Regular },
   setup () {
 
     const { tableColumns, tableData } = useTableListData ();
