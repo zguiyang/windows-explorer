65301b9435f73b2e3fde8bc836f011f2deab7917
only丶炀 <1763645020@qq.com>
2022-10-23 11:52:46 +0800

    feat: 增加默认文件名

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,4 +1,6 @@
-import { FileTextTwotone, FolderFilled } from '@vicons/antd';
+import { FileTextTwotone, FolderFilled, FileImageOutlined } from '@vicons/antd';
+
+import { VideoFileFilled, MusicVideoOutlined } from '@vicons/material';
 
 import { ExplorerFileModel, ExplorerFileTypeEnum, ExplorerOperationEnums } from '@/lib/explorer-type';
 
@@ -9,6 +11,16 @@ export const ROOT_DIR_NAME = '根目录';
 export const NEW_FOLDER_DEFAULT_NAME = '新建文件夹';
 
 export const NEW_TXT_DEFAULT_NAME = '新建文本文件';
+
+export const NEW_VIDEO_DEFAULT_NAME = '新建视频文件';
+
+export const NEW_AUDIO_DEFAULT_NAME = '新建音频文件';
+
+export const NEW_JPG_DEFAULT_NAME = '新建JPG图片';
+
+export const NEW_PNG_DEFAULT_NAME = '新建PNG图片';
+
+export const NEW_UNKNOWN_DEFAULT_NAME = '未知类型文件';
 
 /**
  * 定义通用的文件类型
@@ -28,11 +40,51 @@ export const EXPLORER_FILE_MODEL_MAP:Partial<Record<ExplorerFileTypeEnum, Explor
   TXT: {
     defaultName: NEW_TXT_DEFAULT_NAME,
     fileType: ExplorerFileTypeEnum.TXT,
-    fileTypeText: 'TXT文件',
+    fileTypeText: 'TXT文本',
     fileIcon: FileTextTwotone,
     fileIconProps: {
       size: 28,
       color: '#b4b3b3',
+    },
+  },
+  MP4: {
+    defaultName: NEW_VIDEO_DEFAULT_NAME,
+    fileType: ExplorerFileTypeEnum.MP4,
+    fileTypeText: 'MP4',
+    fileIcon: VideoFileFilled,
+    fileIconProps: {
+      size: 28,
+      color: '#0d7dd1',
+    },
+  },
+  MP3: {
+    defaultName: NEW_AUDIO_DEFAULT_NAME,
+    fileType: ExplorerFileTypeEnum.MP3,
+    fileTypeText: 'MP3',
+    fileIcon: MusicVideoOutlined,
+    fileIconProps: {
+      size: 28,
+      color: '#0d7dd1',
+    },
+  },
+  JPG: {
+    defaultName: NEW_JPG_DEFAULT_NAME,
+    fileType: ExplorerFileTypeEnum.JPG,
+    fileTypeText: 'JPG',
+    fileIcon: FileImageOutlined,
+    fileIconProps: {
+      size: 28,
+      color: '#0d7dd1',
+    },
+  },
+  PNG: {
+    defaultName: NEW_PNG_DEFAULT_NAME,
+    fileType: ExplorerFileTypeEnum.PNG,
+    fileTypeText: 'PNG',
+    fileIcon: FileImageOutlined,
+    fileIconProps: {
+      size: 28,
+      color: '#0d7dd1',
     },
   },
 };
@@ -50,9 +102,11 @@ const EXPLORER_IMAGE_OPERATIONS = [ ...EXPLORER_PUBLIC_OPERATIONS, ExplorerOpera
 // 文件类型操作关系映射
 
 export const EXPLORER_OPERATION_ROLES_MAP = new Map ( [
+  [ ExplorerFileTypeEnum.UNKNOWN, [ ...EXPLORER_PUBLIC_OPERATIONS ] ],
   [ ExplorerFileTypeEnum.FOLDER, [ ...EXPLORER_PUBLIC_OPERATIONS ] ],
   [ ExplorerFileTypeEnum.JPG, [ ...EXPLORER_IMAGE_OPERATIONS ] ],
   [ ExplorerFileTypeEnum.PNG, [ ...EXPLORER_IMAGE_OPERATIONS ] ],
   [ ExplorerFileTypeEnum.TXT, [ ...EXPLORER_PUBLIC_OPERATIONS ] ],
   [ ExplorerFileTypeEnum.MP4, [ ...EXPLORER_PUBLIC_OPERATIONS, ExplorerOperationEnums.VIDEO_PLAY ] ],
+  [ ExplorerFileTypeEnum.MP3, [ ...EXPLORER_PUBLIC_OPERATIONS, ExplorerOperationEnums.AUDIO_PLAY, ExplorerOperationEnums.AUDIO_DOWNLOAD ] ],
 ] );
